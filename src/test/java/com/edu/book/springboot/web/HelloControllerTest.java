6222e0a222b8d4a1c435d2fa4a7002275e1089b5
jimi008285 <jimi008285@gmail.com>
2021-10-20 00:09:20 +0900

    깃 아이디 테스트

--- src/test/java/com/edu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/edu/book/springboot/web/HelloControllerTest.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
+// ResultActions가 아니됨
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
